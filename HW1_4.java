52bc3462367f8622201d818539c94a6248b63a1b
Dmitriy-177 <79001076+Dmitriy-177@users.noreply.github.com>
2021-02-14 17:08:39 +0300

    Add files via upload

--- HW1_4.java
+++ HW1_4.java
@@ -1,7 +1,27 @@
 public class HW1_4 {
-}public static void main(String[] args){
-    // 3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
-    //   где a, b, c, d – аргументы этого метода, имеющие тип float
+    public static void main(String[] args) {
+
+        // Написать метод, принимающий на вход два целых числа и проверяющий,
+        //что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true,
+        //в противном случае – false.
+
+        public static boolean isSumBetween10and20(int a, int b)
+        {
+            int sum = a + b;
+            boolean res = sum > 10 && sum <= 20;
+            return res;
+        }
+    }
 
 }
-}
+
+
+
+
+
+
+
+
+
+
+
---
+++ HW1_5.java
@@ -0,0 +1,17 @@
+public class HW1_5 {
+    public static void main(String[] args) {
+        public static void printNegativeOrPositive ( int number)
+        {
+            if (number >= 0)
+            {
+                System.out.println(number + "положительное число");
+            }
+            else
+            {
+                System.out.println(number + "отрицательное число");
+            }
+
+        }
+
+    }
+}

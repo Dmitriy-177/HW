bab482cfbbdf464998f1e4fa36dbd4616f9cef66
Dmitriy-177 <79001076+Dmitriy-177@users.noreply.github.com>
2021-02-14 17:23:03 +0300

    Add files via upload

---
+++ HW1_6.java
@@ -0,0 +1,7 @@
+public class HW1_6 {
+    public static boolean isNegative(int number)
+    {
+        return number < 0;
+    }
+}
+/*Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.*/
